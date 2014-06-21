9e2729c9c089293158fe903f31ce15768309a622
Dave <dave.idell@gmail.com>
2014-06-21 11:18:35 -0400

    yeoman init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('abcdApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
